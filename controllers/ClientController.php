5bc0a8dee45451c90e9f9ce2c21700cf258bbbbf
Chaineze Said <chachanam24@gmail>
2025-10-09 01:29:46 +0200

    ajout de fichiers

---
+++ controllers/ClientController.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "models/ClientModel.php";
+
+class ClientController
+{
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new ClientModel();
+    }
+
+    public function getAllClients()
+    {
+        $clients = $this->model->getDBAllClients();
+        echo json_encode($clients);
+    }
+}
+//$clientController = new ClientController();
+//$clientController->getAllClients();
